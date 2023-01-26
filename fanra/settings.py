32f3e37968fc1547247c73f1410bc1d118984bb4
vamsi <vamsi.scientistgmail.com>
2023-01-27 00:17:37 +0530

    added CORS_ORIGIN_ALLOW_ALL

--- fanra/settings.py
+++ fanra/settings.py
@@ -125,9 +125,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://fanra-frontend-91kn.vercel.app",
-    "https://connections.fanraa.in"
-]
+CORS_ORIGIN_ALLOW_ALL = True
